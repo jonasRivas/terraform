135ac8d6ba7e60feb776ba43fc3ea92f3b4b48cd
Jonas Rivas <jard.dugarte@gmail.com>
2023-02-02 11:51:59 +0100

    Basic terraform example with AWS as a provider

---
+++ main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  access_key = var.my_access_key 
+  secret_key = var.my_secret_key 
+  region  = "eu-west-1"
+}
+
+locals {
+  setup_name = "tuts"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.5.0.0/16"
+
+  tags = {
+    Name = "${local.setup_name}-vpc"
+    foo = local.setup_name
+  }
+}
+
+resource "aws_subnet" "web" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.5.0.0/16"
+
+  tags = {
+    Name = "${local.setup_name}-subnet"
+  }
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0333305f9719618c7"
+  instance_type = var.my_instance_type 
+
+  tags = {
+    Name = "${local.setup_name}-ExampleFromTerraform"
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "my_instance_type" {
+  type = string
+  description = "default instance type"
+}
+
+variable "my_access_key" {
+  type = string
+  description = "default access key"
+}
+
+variable "my_secret_key" {
+  type = string
+  description = "default secret key"
+}
